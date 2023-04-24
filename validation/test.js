73161c95e87b6bad5a3b8b7f0f9217b6a5c08ad3
Micah Halter <micah@mehalter.com>
2023-04-24 15:37:03 -0400

    Cleanup: use prettier to format test javascript file

--- validation/test.js
+++ validation/test.js
@@ -1,13 +1,13 @@
-const { globSync } = require('glob');
-const Ajv = require('ajv');
-const fs = require('fs');
-const addFormats = require('ajv-formats');
-const path = require('node:path');
+const { globSync } = require("glob");
+const Ajv = require("ajv");
+const fs = require("fs");
+const addFormats = require("ajv-formats");
+const path = require("node:path");
 
 const ajv = new Ajv();
 addFormats(ajv);
 
-const testPath = (process.argv.length > 2) ? process.argv[2] : "../";
+const testPath = process.argv.length > 2 ? process.argv[2] : "../";
 
 const baseSchemaFile = `${testPath}base_schema.json`;
 const schemaFiles = globSync(`${testPath}*/*.json`);
@@ -16,18 +16,19 @@ const validate = (schemaFile, objectFile) => {
     const object = JSON.parse(fs.readFileSync(objectFile));
     const schema = JSON.parse(fs.readFileSync(schemaFile));
     const validated = ajv.validate(schema, object);
-    const passValue = (validated ? 'PASS' : 'FAIL');
-    console.log(`${objectFile} <- ${schemaFile} : ${passValue}`)
+    const passValue = validated ? "PASS" : "FAIL";
+    console.log(`${objectFile} <- ${schemaFile} : ${passValue}`);
     return validated;
-}
-
+};
 
 let passed = true;
 let testCount = 0;
 let passCount = 0;
 for (let schemaFile of schemaFiles) {
     // TODO: Validate each model schema matches the base schema.
-    const exampleFiles = globSync(path.join(path.dirname(schemaFile), "examples/*.json"));
+    const exampleFiles = globSync(
+        path.join(path.dirname(schemaFile), "examples/*.json")
+    );
     for (let exampleFile of exampleFiles) {
         testCount++;
         const baseSchemaPassed = validate(baseSchemaFile, exampleFile);
@@ -36,18 +37,18 @@ for (let schemaFile of schemaFiles) {
         const testPassed = baseSchemaPassed & filePassed;
         if (testPassed) {
             passCount++;
+        } else {
+            passed = false;
         }
-        passed = passed & testPassed;
     }
 }
 
 // Returns a response code if all examples validate, otherwise a 1.
+console.log(`\n${passCount} out of ${testCount} tests passed.`);
 if (!passed) {
-    console.log('\nError validating schemas. See above.');
+    console.log("\nError validating schemas. See above.");
     process.exit(1);
-}
-else {
-    console.log(`\n${passCount} out of ${testCount} tests passed.`);
-    console.log('\nAll schemas validated.');
+} else {
+    console.log("\nAll schemas validated.");
     process.exit(0);
 }
